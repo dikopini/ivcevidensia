e464a1a7934d6e7ba56de2e7ec3958bede2382d3
irfan_basyar <irfanbasyar.ib@gmail.com>
2023-04-12 18:58:15 +0700

    start

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ from bs4 import BeautifulSoup
 url = 'https://ivcevidensia.co.uk/ems-choose-your-preferred-location'
 
 res = requests.get(url)
-soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.prettify())
+print(res.text)
+#soup = BeautifulSoup(res.text, 'html.parser')
+#content = soup.find('div', 'jss166 jss167')
+#item = soup.find_all('div', class_= ['jss244 jss245', 'jss244 jss260'])
